437c6c5d2cc36b4c0f86ad8e8c54deb723df3ce4
JamieVerner123 <rambo22212@hotmail.co.uk>
2015-11-19 14:27:56 +0000

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,43 +1,37 @@
 var paused_count =0;
 var resumed_count = 0;
 var launched_count = 0;
+        var key = "pen", "colour", "food", "drink";
+        var value = "blue" , "red", "burger", "coke";
+        
+        window.localStorage.setItem(key, value);
 
     function onLoad() {
         document.addEventListener("deviceready", onDeviceReady, false);
 		console.log("device ready");
     }
 	
-/*	function updateDisplay() {
+	function updateDisplay() {
 		$("#launched").text("Application launched: " + launched_count);
 		$("#resumed").text("Application paused: " + paused_count);
 		$("#paused").text("Application resumed: " + resumed_count);
 	}
-*/
+
 
     // device APIs are available
     //
     function onDeviceReady() {
 		alert("device ready");
         
-        var key = "pen", "colour", "food", "drink";
-        var value = "blue" , "red", "burger", "coke";
-        
-        window.localStorage.setItem(key, value);
         
         alert(window.localStorage.getItem(key));
         
         
-        
-        
-        
-        
-        
-        /*
 		document.addEventListener("resume", onResume, false);
 		document.addEventListener("pause", onPause, false);
 		
 		launched_count++;
-		updateDisplay(); */
+		updateDisplay(); 
     }
 
     // Handle the pause event
