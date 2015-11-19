7382cea7e2e476e999b4c8861008301609869bed
JamieVerner123 <rambo22212@hotmail.co.uk>
2015-11-19 15:06:25 +0000

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -19,16 +19,33 @@ var launched_count = 0;
 		$("#resumed").text("Application paused: " + paused_count);
 		$("#paused").text("Application resumed: " + resumed_count);
 	}
+	        var db = window.openDatabase("food", 1.0, "food database", 1024 * 1024);
+db.transaction(createTable, error, success);
+	
+function createTable(tx) {	
+ 	tx.executeSql("CREATE TABLE IF NOT EXISTS food(id UNIQUE, title TEXT)");
+}
+	
+function success() {
+	db.transaction(addFood, error);
+}
+	
+function addFood(tx) {
+	tx.executeSql("INSERT INTO food(id, title) VALUES (1, 'chips')");
+	tx.executeSql("INSERT INTO food(id, title) VALUES (2, 'cake')");
+	tx.executeSql("INSERT INTO food(id, title) VALUES (3, 'nachos')");
+}
+	
+function error(err) {
+	alert("oops something went wrong " + err.message);
+}
 
 
     // device APIs are available
     //
     function onDeviceReady() {
 		alert("device ready");
-        
-        var value = "red";
-        var key = alert(window.localStorage.length);
-        
+
         
 		document.addEventListener("resume", onResume, false);
 		document.addEventListener("pause", onPause, false);
