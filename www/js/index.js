11c923a1ea8a6125fb5fd23c6b216ead735533f3
JamieVerner123 <rambo22212@hotmail.co.uk>
2015-11-23 11:24:40 +0000

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -17,6 +17,8 @@ var launched_count = 0;
 		$("#launched").text("Application launched: " + launched_count);
 		$("#resumed").text("Application paused: " + paused_count);
 		$("#paused").text("Application resumed: " + resumed_count);
+		$("#paused").text("Application location: " + location);
+
 	}
 	
 
